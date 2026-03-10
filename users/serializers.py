d316362c892f6f776525d5ad85576cd7a06071a1
Sakir Mohammad <safayetsakirmohammad@gmail.com>
2026-03-10 16:22:17 +0600

    is staff add

--- users/serializers.py
+++ users/serializers.py
@@ -14,5 +14,6 @@ class UserSerializer(BaseUserSerializer):
     class Meta(BaseUserSerializer.Meta):
         ref_name = 'CustomUser'
         fields = [
-            'id', 'email', 'first_name', 'last_name', 'address', 'phone_number'
+            'id', 'email', 'first_name', 'last_name', 'address', 'phone_number','is_staff'
         ]
+        read_only_fields=['is_staff']
